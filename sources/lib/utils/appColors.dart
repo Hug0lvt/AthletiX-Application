078a43797c65b4d363ee4fcc9d293a48ba6777e5
zakariya.saoula <zakariya.saoula@etu.uca.fr>
2024-01-16 17:03:56 +0100

    :sparkles: message and discussion pages

---
+++ sources/lib/utils/appColors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final AppColors _instance = AppColors._internal();
+
+  factory AppColors() {
+    return _instance;
+  }
+
+  AppColors._internal();
+
+  static const Color greyDark = Color(0xFF282828);
+  static const Color black = Color(0xFF232323);
+  static const Color grey = Color(0xFF323232);
+  static const Color blackLight = Color(0xE51A1A1A);
+  static const Color textGrey = Color(0xFFBABABA);
+  static const Color greyLight = Color(0x80FFFFFF);
+  static const Color textBlueLight = Color(0xFFA2E4E6);
+  static const Color violet = Color(0xFFB66CFF);
+}
