4867675076a6d74fdd9f754806c56b98ee606651
mohamed <mohamed.hassani@etu.uca.fr>
2024-02-05 17:24:20 +0100

    Training liaison API OK, reste a tester!

--- sources/lib/page/trainingTabs/TrainingTab.dart
+++ sources/lib/page/trainingTabs/TrainingTab.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:sources/providers/api/api_client.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../components/ProgramContainer.dart';
+import '../../model/session.dart';
+import '../../providers/api/session_api_client.dart';
 
-class TrainingTab extends StatelessWidget {
+class TrainingApiClient extends ApiClient with SessionApiClient {
+  TrainingApiClient(String baseUrl) : super(baseUrl);
+}
+
+class TrainingTab extends StatefulWidget {
   const TrainingTab({super.key});
+  @override
+  State<TrainingTab> createState() => _TrainingTab();
+}
 
+class _TrainingTab extends State<TrainingTab> {
   get onPressed => null;
+
+  final TrainingApiClient apiClient =
+  TrainingApiClient("https://codefirst.iut.uca.fr/containers/AthletiX-ath-api/");
+  late Future<List<Session>> FutureSessions;
+
+  String searchQuery = ''; // Add this line to store the search query
+
+  @override
+  void initState() {
+    super.initState();
+    FutureSessions = apiClient.getAllSessions();
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body:
-      Container(
+      body: Container(
         color: Color(0xFF282828),
-        child:
-        Column(
+        child: Column(
           children: [
             const SizedBox(height: 8.0),
             SizedBox(
@@ -24,6 +45,11 @@ class TrainingTab extends StatelessWidget {
               child: Stack(
                 children: [
                   TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        searchQuery = value;
+                      });
+                    },
                     style: const TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       hintText: 'Search',
@@ -36,7 +62,7 @@ class TrainingTab extends StatelessWidget {
                         borderRadius: BorderRadius.circular(50),
                       ),
                       prefixIcon: Padding(
-                        padding: const EdgeInsets.all(15.0), // Adjust padding as needed
+                        padding: const EdgeInsets.all(15.0),
                         child: SvgPicture.asset(
                           'assets/MagGlass.svg',
                         ),
@@ -47,6 +73,7 @@ class TrainingTab extends StatelessWidget {
                 ],
               ),
             ),
+            const SizedBox(height: 8.0),
             Row(
               children: [
                 const SizedBox(width: 8.0),
@@ -54,7 +81,8 @@ class TrainingTab extends StatelessWidget {
                   'My Training Programs',
                   style: TextStyle(
                     color: Colors.white,
-                    fontFamily: 'Mulish',),
+                    fontFamily: 'Mulish',
+                  ),
                 ),
                 const SizedBox(width: 8.0),
                 Expanded(
@@ -63,36 +91,37 @@ class TrainingTab extends StatelessWidget {
                     height: 1.0,
                   ),
                 ),
-                IconButton(onPressed: onPressed, icon: SvgPicture.asset('assets/AddPlus.svg'),)
+                IconButton(
+                  onPressed: onPressed,
+                  icon: SvgPicture.asset('assets/AddPlus.svg'),
+                )
               ],
             ),
             const SizedBox(height: 8.0),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  ProgramContainer(
-                    title: 'Push',
-                    lastSession: '19',
-                    exercises: [
-                      '4 x 8 Dumbbell Benchpress',
-                      '4 x 8 Inclined Dumbbell Benchpress',
-                      '4 x 8 Machine Fly',
-                      '4 x 8 Cable Triceps',
-                    ],
-                  ),
-                  ProgramContainer(
-                    title: 'Pull',
-                    lastSession: '1',
-                    exercises: [
-                      '4 x 8 Pull-ups',
-                      '4 x 8 Barbell Rows',
-                      '4 x 8 Lat Pulldowns',
-                      '4 x 8 Face Pulls',
-                    ],
-                  ),
-                ],
-              ),
+            FutureBuilder<List<Session>>(
+              future: FutureSessions,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Session> allSessions = snapshot.data!;
+                  List<Session> filteredSessions = allSessions
+                      .where((session) =>
+                      session.name.toLowerCase().contains(searchQuery.toLowerCase()))
+                      .toList();
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: filteredSessions.length,
+                    itemBuilder: (context, index) {
+                      return ProgramContainer(
+                        title: filteredSessions[index].name,
+                        lastSession: '',
+                        exercises: filteredSessions[index].exercises,
+                      );
+                    },
+                  );
+                }
+                return const CircularProgressIndicator();
+              },
             ),
           ],
         ),
