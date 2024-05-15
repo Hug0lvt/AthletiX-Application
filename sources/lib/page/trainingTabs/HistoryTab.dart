d382b703117d44dc90b7e3f9be2c71a525ee2047
mohamed <mohamed.hassani@etu.uca.fr>
2024-05-15 12:03:18 +0200

    Change FutureSessions en sessions en async, + load data async

--- sources/lib/page/trainingTabs/HistoryTab.dart
+++ sources/lib/page/trainingTabs/HistoryTab.dart
@@ -1,3 +1,4 @@
+import 'package:AthletiX/exceptions/not_found_exception.dart';
 import 'package:AthletiX/model/profile.dart';
 import 'package:AthletiX/providers/localstorage/secure/authManager.dart';
 import 'package:flutter/material.dart';
@@ -19,28 +20,45 @@ class _HistoryTab extends State<HistoryTab> {
 
   get onPressed => null;
 
-  late Future<List<Session>> FutureSessions;
+  late List<Session> sessions;
+
+  bool isLoading = false;
 
   String searchQuery = '';
 
   @override
   void initState() {
+    sessions = [];
     super.initState();
-    int? profileId;
-    Future<Profile?> profileFuture = AuthManager.getProfile();
-    FutureBuilder<Profile?>(
-      future: profileFuture,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            Profile? profile = snapshot.data!;
-            profileId = profile.id;
-          }
-          return const CircularProgressIndicator();
-        },
-    );
-    FutureSessions = clientApi.getSessionsOfUser(profileId);
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      _loadSessions();
+    });
 
   }
+  void _loadSessions() async {
+    setState(() {
+      isLoading = true;
+    });
+    int? profileId;
+    Profile? profile = await AuthManager.getProfile();
+    if (profile != null) {
+      profileId = profile.id;
+    }
+    try {
+      List<Session> fetchedSessions = await clientApi.getSessionsOfUser(profileId);
+      setState(() {
+        sessions = fetchedSessions;
+        isLoading = false;
+      });
+    } on NotFoundException catch (_) {
+      // Gère spécifiquement la NotFoundException
+      setState(() {
+        sessions = []; // Aucune session trouvée
+        isLoading = false;
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -71,62 +89,38 @@ class _HistoryTab extends State<HistoryTab> {
               ],
             ),
             const SizedBox(height: 8.0),
-          ListView(
-            shrinkWrap: true,
-            children: [
-              FutureBuilder<List<Session>>(
-                future: FutureSessions,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    List<Session> allSessions = snapshot.data!;
-                    for (int i = 0; i < allSessions.length; i++) {
-                      ProgramContainer(
-                        title: allSessions[i].name,
-                        lastSession : DateTime.now().difference(allSessions[i].date).inDays.toString(), // in days
-                        exercises : allSessions[i].exercises
-                      );
-                    }
-                  }
-                  return const Center(
-                    child: Text(
-                      "No Past Sessions Found",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Mulish',
-                      ),
+            ListView(
+              shrinkWrap: true,
+              children: [
+                isLoading
+                    ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : sessions.isEmpty
+                    ? const Center(
+                  child: Text(
+                    "No Past Sessions Found",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Mulish',
                     ),
-                  ) ;
-                },
-              )
-          ],
-              /*ListView(
-                shrinkWrap: true,
-                children:*/
-
-                /*[
-                  ProgramContainer(
-                    title: 'Push',
-                    lastSession: '19',
-                    exercises: [
-                      '4 x 8 Dumbbell Benchpress',
-                      '4 x 8 Inclined Dumbbell Benchpress',
-                      '4 x 8 Machine Fly',
-                      '4 x 8 Cable Triceps',
-                    ],
                   ),
-                  ProgramContainer(
-                    title: 'Pull',
-                    lastSession: '1',
-                    exercises: [
-                      '4 x 8 Pull-ups',
-                      '4 x 8 Barbell Rows',
-                      '4 x 8 Lat Pulldowns',
-                      '4 x 8 Face Pulls',
-                    ],
-                  ),
-                  // Add more ProgramContainer widgets as needed
-                ],
-              ),*/
+                )
+                    : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: sessions.length,
+                  itemBuilder: (context, index) {
+                    return ProgramContainer(
+                      title: sessions[index].name,
+                      lastSession: DateTime.now()
+                          .difference(sessions[index].date)
+                          .inDays
+                          .toString(), // in days
+                      exercises: sessions[index].exercises,
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
