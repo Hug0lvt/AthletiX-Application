a3ee06657d86cae7300a24c6dd1fa792a723a88f
alexandre <Alexandre.glenat2@gmail.com>
2024-06-07 11:25:59 +0200

    :bug: correction profile private

--- sources/lib/page/profilePrivate.dart
+++ sources/lib/page/profilePrivate.dart
@@ -15,7 +15,8 @@ class ProfilePrivatePage extends StatefulWidget {
 
 class _ProfilePrivatePageState extends State<ProfilePrivatePage> {
   Profile? _profile;
-
+  bool _isLoading = true;
+  
   @override
   void initState() {
     super.initState();
@@ -28,6 +29,7 @@ class _ProfilePrivatePageState extends State<ProfilePrivatePage> {
 
     setState(() {
       _profile = profile;
+      _isLoading = false;
     });
   }
 
@@ -86,197 +88,201 @@ class _ProfilePrivatePageState extends State<ProfilePrivatePage> {
     }
 
     return Scaffold(
+      backgroundColor: Color(0xFF363636),
       body: SafeArea(
-        child: Container(
-          color: const Color(0xFF363636),
-          padding: const EdgeInsets.all(36),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      margin: const  EdgeInsets.fromLTRB(0, 40, 0, 10),
-                      constraints: BoxConstraints.expand(
-                        width: screenWidth * 0.9,
-                        height: screenHeight * 0.2,
-                      ),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1A1A1A).withOpacity(0.7),
-                        borderRadius: BorderRadius.circular(25.0)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            _profile?.username.toString() ?? 'Unknown',
-                            style: TextStyle(
-                              fontSize: ((screenWidth + screenHeight) / 2) * 0.040,
-                              color: Colors.white,
+        child: _isLoading // Afficher l'indicateur de chargement si _isLoading est vrai
+            ? Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        )
+            : SingleChildScrollView(
+          child: Container(
+            color: const Color(0xFF363636),
+            padding: const EdgeInsets.all(36),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 40, 0, 10),
+                        constraints: BoxConstraints.expand(
+                          width: screenWidth * 0.9,
+                          height: screenHeight * 0.2,
+                        ),
+                        decoration: BoxDecoration(
+                            color: const Color(0xFF1A1A1A).withOpacity(0.7),
+                            borderRadius: BorderRadius.circular(25.0)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              _profile?.username.toString() ?? 'Unknown',
+                              style: TextStyle(
+                                fontSize: ((screenWidth + screenHeight) / 2) * 0.040,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          Container(
-                            width: screenWidth * 0.3,
-                            height: 1.0,
-                            color: const Color(0xFF434343),
-                            margin: const EdgeInsets.symmetric(vertical: 5.0),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Column(
-                                children: [
-                                  Text(
-                                    'Gender',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                            Container(
+                              width: screenWidth * 0.3,
+                              height: 1.0,
+                              color: const Color(0xFF434343),
+                              margin: const EdgeInsets.symmetric(vertical: 5.0),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Gender',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    _profile!.gender! ? "Male" : "Female",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                    Text(
+                                      _profile!.gender! ? "Male" : "Female",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    'Age',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Age',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    _profile?.age.toString() ?? 'Unknown',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                    Text(
+                                      _profile?.age.toString() ?? 'Unknown',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    'Height',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Height',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    (_profile?.height.toString() ?? '0') + ' cm',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                    Text(
+                                      (_profile?.height.toString() ?? '0') + ' cm',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    'Weight',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      'Weight',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    (_profile?.weight.toString() ?? '0') + ' kg',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: screenWidth * 0.034,
+                                    Text(
+                                      (_profile?.weight.toString() ?? '0') + ' kg',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: screenWidth * 0.034,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      top: 50.0,
-                      right: 20.0,
-                      child: IconButton(
-                        onPressed: onPressed,
-                        icon: SvgPicture.asset(
-                          'assets/EditIcon.svg',
-                          width: screenWidth * 0.034,
-                          height: screenHeight * 0.034,
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    // Image en haut au centre
-                    Positioned(
-                      top: 0,
-                      child: ClipOval(
-                        child: _buildProfileImage(screenWidth)
+                      Positioned(
+                        top: 50.0,
+                        right: 20.0,
+                        child: IconButton(
+                          onPressed: onPressed,
+                          icon: SvgPicture.asset(
+                            'assets/EditIcon.svg',
+                            width: screenWidth * 0.034,
+                            height: screenHeight * 0.034,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ButtonProfilePage(
-                      text: 'Posts',
-                      imagePath: 'assets/PostIcon.svg',
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.07,
-                      screenHeight: screenHeight,
-                      screenWidth: screenWidth,
-                    ),
-                    ButtonProfilePage(
-                      text: 'Favorite Posts',
-                      imagePath: 'assets/HeartIcon.svg',
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.07,
-                      screenHeight: screenHeight,
-                      screenWidth: screenWidth,
-                    ),
-                    ButtonProfilePage(
-                      text: 'Statistics',
-                      imagePath: 'assets/StatisticsIcon.svg',
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.07,
-                      screenHeight: screenHeight,
-                      screenWidth: screenWidth,
-                    ),
-                    ButtonProfilePage(
-                      text: 'Exercises',
-                      imagePath: 'assets/DoubleHaltereIcon.svg',
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.07,
-                      screenHeight: screenHeight,
-                      screenWidth: screenWidth,
-                    ),
-                    ButtonProfilePage(
-                      text: 'Settings',
-                      imagePath: 'assets/SettingsIcon.svg',
-                      width: screenWidth * 0.9,
-                      height: screenHeight * 0.07,
-                      screenHeight: screenHeight,
-                      screenWidth: screenWidth,
-                    ),
-                  ],
-                ),
-              ],
+                      // Image en haut au centre
+                      Positioned(
+                        top: 0,
+                        child: ClipOval(child: _buildProfileImage(screenWidth)),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ButtonProfilePage(
+                        text: 'Posts',
+                        imagePath: 'assets/PostIcon.svg',
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.07,
+                        screenHeight: screenHeight,
+                        screenWidth: screenWidth,
+                      ),
+                      ButtonProfilePage(
+                        text: 'Favorite Posts',
+                        imagePath: 'assets/HeartIcon.svg',
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.07,
+                        screenHeight: screenHeight,
+                        screenWidth: screenWidth,
+                      ),
+                      ButtonProfilePage(
+                        text: 'Statistics',
+                        imagePath: 'assets/StatisticsIcon.svg',
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.07,
+                        screenHeight: screenHeight,
+                        screenWidth: screenWidth,
+                      ),
+                      ButtonProfilePage(
+                        text: 'Exercises',
+                        imagePath: 'assets/DoubleHaltereIcon.svg',
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.07,
+                        screenHeight: screenHeight,
+                        screenWidth: screenWidth,
+                      ),
+                      ButtonProfilePage(
+                        text: 'Settings',
+                        imagePath: 'assets/SettingsIcon.svg',
+                        width: screenWidth * 0.9,
+                        height: screenHeight * 0.07,
+                        screenHeight: screenHeight,
+                        screenWidth: screenWidth,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-
-
 }
