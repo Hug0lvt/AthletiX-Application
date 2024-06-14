333fca134e9fce0d2694898af93dee27a0896733
mohamed <mohamed.hassani@etu.uca.fr>
2024-06-14 13:25:57 +0200

    :sparkles: thumbnail 50% done

--- sources/lib/components/profilePublicPostedCard.dart
+++ sources/lib/components/profilePublicPostedCard.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+import 'dart:typed_data';
 
-class ProfilePublicPostedCard extends StatelessWidget {
-  final String imagePath;
+class ProfilePublicPostedCard extends StatefulWidget {
+  final String videoUrl;
   final String postName;
   //final String postDate;
   final String description;
@@ -9,7 +11,7 @@ class ProfilePublicPostedCard extends StatelessWidget {
   final double height;
 
   ProfilePublicPostedCard({
-    required this.imagePath,
+    required this.videoUrl,
     required this.postName,
     //required this.postDate,
     required this.description,
@@ -18,10 +20,39 @@ class ProfilePublicPostedCard extends StatelessWidget {
   });
 
   @override
+  _ProfilePublicPostedCardState createState() => _ProfilePublicPostedCardState();
+}
+
+class _ProfilePublicPostedCardState extends State<ProfilePublicPostedCard> {
+  Uint8List? _thumbnail;
+
+  @override
+  void initState() {
+    super.initState();
+    _generateThumbnail();
+  }
+
+  Future<void> _generateThumbnail() async {
+    try {
+      final thumbnail = await VideoThumbnail.thumbnailData(
+        video: widget.videoUrl,
+        imageFormat: ImageFormat.JPEG,
+        maxHeight: 100, // specify the height of the thumbnail, width is auto-scaled to keep aspect ratio
+        quality: 75, // specify the quality of the thumbnail
+      );
+      setState(() {
+        _thumbnail = thumbnail;
+      });
+    } catch (e) {
+      print('Error generating thumbnail: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      width: width,
-      height: height,
+      width: widget.width,
+      height: widget.height,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: const Color(0xe51a1a1a),
@@ -34,9 +65,9 @@ class ProfilePublicPostedCard extends StatelessWidget {
             Align(
               alignment: Alignment.center,
               child: Text(
-                postName,
+                widget.postName,
                 style: TextStyle(
-                  fontSize: width * 0.05,
+                  fontSize: widget.width * 0.05,
                   fontWeight: FontWeight.w500,
                   color: Colors.white,
                 ),
@@ -44,25 +75,32 @@ class ProfilePublicPostedCard extends StatelessWidget {
             ),
             ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                imagePath,
+              child: _thumbnail != null
+                  ? Image.memory(
+                _thumbnail!,
                 width: double.infinity,
-                height: height * 0.8,
+                height: widget.height * 0.8,
+                fit: BoxFit.cover,
+              )
+                  : Image.network(
+                'https://via.placeholder.com/800/3C383B/B66CFF?text=No+Thumbnail',
+                width: double.infinity,
+                height: widget.height * 0.8,
                 fit: BoxFit.cover,
               ),
             ),
             /*Text(
-              postDate,
+              widget.postDate,
               style: TextStyle(
-                fontSize: width * 0.03,
+                fontSize: widget.width * 0.03,
                 fontWeight: FontWeight.w500,
                 color: Colors.grey,
               ),
             ),*/
             Text(
-              description,
+              widget.description,
               style: TextStyle(
-                fontSize: width * 0.03,
+                fontSize: widget.width * 0.03,
                 fontWeight: FontWeight.w500,
                 color: Colors.white,
               ),
@@ -70,7 +108,7 @@ class ProfilePublicPostedCard extends StatelessWidget {
             Text(
               "Read More",
               style: TextStyle(
-                fontSize: width * 0.03,
+                fontSize: widget.width * 0.03,
                 fontWeight: FontWeight.w500,
                 color: Colors.blue,
               ),
--- sources/lib/page/myPosts.dart
+++ sources/lib/page/myPosts.dart
@@ -2,11 +2,6 @@ import 'dart:convert';
 import 'package:AthletiX/providers/api/clientApi.dart';
 import 'package:AthletiX/providers/api/utils/postClientApi.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:video_thumbnail/video_thumbnail.dart';
-import 'dart:typed_data';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import '../components/profilePublicPostedCard.dart';
 import '../components/sendMessage.dart';
 import '../main.dart';
@@ -24,61 +19,43 @@ class _MyPostPageState extends State<MyPostPage> {
   final _clientApi = getIt<ClientApi>();
   Profile? _profile;
   List<Post> _posts = [];
-  List<String> _thumbnails = [];
   bool _isLoadingProfile = true;
 
   @override
   void initState() {
     super.initState();
     fetchProfile();
+    fetchVideos();
   }
 
   Future<void> fetchProfile() async {
-    Profile? profile = await AuthManager.getProfile();
-    setState(() {
-      _profile = profile;
-      _isLoadingProfile = false;
-    });
-    if (_profile != null) {
-      fetchMyPosts();
+    try {
+      Profile? profile = await AuthManager.getProfile();
+      setState(() {
+        _profile = profile;
+        _isLoadingProfile = false;
+      });
+      if (_profile != null) {
+        //fetchMyPosts();
+      }
+    } catch (e) {
+      setState(() {
+        _isLoadingProfile = false;
+      });
+      // Handle error appropriately
+      print("Error fetching profile: $e");
     }
   }
 
-  Future<void> fetchMyPosts() async {
-    if (_profile?.id != null) {
-      int profileId = _profile!.id!;
-      String jsonReply = await _clientApi.getData('posts/user/$profileId?includeComments=true&includeExercises=true');
-      Map<String, dynamic> data = json.decode(jsonReply);
-      String jsonItems = json.encode(data["items"]);
-      _posts = postFromJson(jsonItems) as List<Post>;
-      _generateThumbnails();
+  Future<void> fetchVideos() async {
+    try {
+      List<Post> posts = await postsClientApi.getRecommendedPosts('1');
+      setState(() {
+        _posts = posts;
+      });
+    } catch (e) {
+      print('Failed to load videos: $e');
     }
-  }
-
-  Future<void> _generateThumbnails() async {
-    List<String> videoUrls = _posts.map((post) => post.content!).toList();
-    List<String> thumbnails = await getThumbnails(videoUrls);
-    setState(() {
-      _thumbnails = thumbnails;
-    });
-  }
-
-  Future<List<String>> getThumbnails(List<String> videoUrls) async {
-    final tempDir = await getTemporaryDirectory();
-    List<String> thumbnailPaths = [];
-
-    for (String videoUrl in videoUrls) {
-      final thumbnailPath = await VideoThumbnail.thumbnailFile(
-        video: videoUrl,
-        thumbnailPath: tempDir.path,
-        imageFormat: ImageFormat.JPEG,
-        maxHeight: 100, // specify the height of the thumbnail, width is auto-scaled to keep aspect ratio
-        quality: 75, // specify the quality of the thumbnail
-      );
-      thumbnailPaths.add(thumbnailPath ?? 'https://via.placeholder.com/800/3C383B/B66CFF?text=No+Thumbnail');
-    }
-
-    return thumbnailPaths;
   }
 
   @override
@@ -97,72 +74,49 @@ class _MyPostPageState extends State<MyPostPage> {
             : Container(
           color: const Color(0xFF363636),
           padding: const EdgeInsets.all(36),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 1.0,
-                        color: const Color(0xFFFFFFFF),
-                      ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 1.0,
+                      color: const Color(0xFFFFFFFF),
                     ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: const Text(
-                        'Posts',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 1.0,
-                        color: const Color(0xFFFFFFFF),
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: (_posts.length / 2).ceil(),
-                    itemBuilder: (context, index) {
-                      int startIndex = index * 2;
-                      int endIndex = startIndex + 1;
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 2.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            if (startIndex < _posts.length)
-                              ProfilePublicPostedCard(
-                                postName: _posts[startIndex].title!,
-                                description: _posts[startIndex].description!,
-                                //postDate: _posts[startIndex].,
-                                imagePath: _thumbnails.isNotEmpty ? _thumbnails[startIndex] : '',
-                                width: screenWidth * 0.42,
-                                height: screenHeight * 0.3,
-                              ),
-                            if (endIndex < _posts.length)
-                              ProfilePublicPostedCard(
-                                postName: _posts[endIndex].title!,
-                                description: _posts[endIndex].description!,
-                                //postDate: _posts[endIndex].,
-                                imagePath: _thumbnails.isNotEmpty ? _thumbnails[endIndex] : '',
-                                width: screenWidth * 0.42,
-                                height: screenHeight * 0.3,
-                              ),
-                          ],
-                        ),
-                      );
-                    },
                   ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: const Text(
+                      'Posts',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 1.0,
+                      color: const Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _posts.length,
+                  itemBuilder: (context, index) {
+                    final post = _posts[index];
+                    return ProfilePublicPostedCard(
+                      postName: post.title ?? 'No Title',
+                      description: post.description ?? 'No Description',
+                      videoUrl: post.content!, // Pass video URL to the card
+                      width: screenWidth * 0.42,
+                      height: screenHeight * 0.3,
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
