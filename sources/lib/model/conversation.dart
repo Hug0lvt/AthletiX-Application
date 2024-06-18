62379511980676e0e73c8dcec32b91024434ccaf
alexandre <Alexandre.glenat2@gmail.com>
2024-06-19 00:49:23 +0200

    :sparkles: connect messages page with backend

--- sources/lib/model/conversation.dart
+++ sources/lib/model/conversation.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:AthletiX/model/profile.dart';
-
 import 'message.dart';
 
 List<Conversation> conversationListFromJson(String str) => List<Conversation>.from(json.decode(str).map((x) => Conversation.fromJson(x)));
@@ -10,24 +8,32 @@ String conversationToJson(Conversation data) => json.encode(data.toJson());
 
 class Conversation {
   int id;
-  List<Profile> profiles;
-  List<Message> messages;
+  String? name;
+  String? picture;
+  List<Profile>? profiles;
+  List<Message>? messages;
 
   Conversation({
     required this.id,
-    required this.profiles,
-    required this.messages,
+    this.name,
+    this.picture,
+    this.profiles,
+    this.messages,
   });
 
   factory Conversation.fromJson(Map<String, dynamic> json) => Conversation(
     id: json["id"],
-    profiles: List<Profile>.from(json["profiles"].map((x) => Profile.fromJson(x))),
-    messages: List<Message>.from(json["messages"].map((x) => Message.fromJson(x))),
+    name: json["name"],
+    picture: json["picture"],
+    profiles: json["profiles"] != null ? List<Profile>.from(json["profiles"].map((x) => Profile.fromJson(x))) : [],
+    messages: json["messages"] != null ? List<Message>.from(json["messages"].map((x) => Message.fromJson(x))) : [],
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
-    "profiles": List<dynamic>.from(profiles.map((x) => x.toJson())),
-    "messages": List<dynamic>.from(messages.map((x) => x.toJson())),
+    "name": name,
+    "picture": picture,
+    "profiles": List<dynamic>.from(profiles!.map((x) => x.toJson())),
+    "messages": List<dynamic>.from(messages!.map((x) => x.toJson())),
   };
 }
