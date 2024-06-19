929a3654d5b566c29b908165d4b3f97ff1934338
mohamed <mohamed.hassani@etu.uca.fr>
2024-06-19 09:08:10 +0200

    :sparkles: search page logic and view done ! needs to be tested with new API

--- sources/lib/page/search.dart
+++ sources/lib/page/search.dart
@@ -1,7 +1,10 @@
 import 'package:AthletiX/components/UserCard.dart';
 import 'package:AthletiX/components/profilePublicPostedCard.dart';
+import 'package:AthletiX/main.dart';
 import 'package:AthletiX/model/post.dart';
 import 'package:AthletiX/model/profile.dart';
+import 'package:AthletiX/providers/api/utils/postClientApi.dart';
+import 'package:AthletiX/providers/api/utils/profileClientApi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -11,16 +14,62 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final postsClientApi = getIt<PostClientApi>();
+  final profileClientApi = getIt<ProfileClientApi>();
   String searchQuery = '';
   bool isPostsTab = true;
-  List<Post> posts = [Post(id: 0), Post(id: 1), Post(id: 2)]; // Replace with actual data
-  List<Profile> users = [Profile(), Profile(), Profile()]; // Replace with actual data
+
+  List<Post> posts = [];
+  bool _isLoadingPosts = true;
+
+  List<Profile> users = [];
+  bool _isLoadingUsers = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    try {
+      List<Profile> fetchedUsers = await profileClientApi.getProfileByQuery(searchQuery);
+      setState(() {
+        users = fetchedUsers;
+        _isLoadingUsers = false;
+      });
+    } catch (e) {
+      print('Failed to load users: $e');
+      setState(() {
+        _isLoadingUsers = false;
+      });
+    }
+  }
+
+  Future<void> fetchPosts() async {
+    try {
+      List<Post> fetchedPosts = await postsClientApi.getPostsByQuery(searchQuery);
+      setState(() {
+        posts = fetchedPosts;
+        _isLoadingPosts = false;
+      });
+    } catch (e) {
+      print('Failed to load posts: $e');
+      setState(() {
+        _isLoadingPosts = false;
+      });
+    }
+  }
 
   void _onSearchChanged(String value) {
     setState(() {
       searchQuery = value;
-      // Apply search logic here
+      _isLoadingPosts = true;
+      _isLoadingUsers = true;
     });
+    fetchPosts();
+    fetchUsers();
   }
 
   void _toggleTab(bool isPosts) {
@@ -36,7 +85,7 @@ class _SearchPageState extends State<SearchPage> {
 
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.fromLTRB(0,screenHeight * 0.05,0,0),
+        padding: EdgeInsets.fromLTRB(0, screenHeight * 0.05, 0, 0),
         color: const Color(0xFF282828),
         child: Column(
           children: [
@@ -125,34 +174,60 @@ class _SearchPageState extends State<SearchPage> {
             const SizedBox(height: 8.0),
             Expanded(
               child: isPostsTab
-                  ? ListView.builder(
+                  ? _isLoadingPosts
+                  ? Center(child: CircularProgressIndicator())
+                  : posts.isEmpty
+                  ? Center(
+                child: Text(
+                  "No Posts Found",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              )
+                  : ListView.builder(
                 itemCount: posts.length,
                 itemBuilder: (context, index) {
-                  return
-                    Container(
-                      margin: EdgeInsets.fromLTRB(screenWidth * 0.01, screenHeight * 0.01, screenWidth * 0.01, 0),
-                  width: screenWidth,
-                  child: ProfilePublicPostedCard(
-                    postName: posts[index]?.publisher?.username ?? "no name",
-                    description: posts[index].description ?? "no description",
-                    postDate: '',
-                    imagePath: '',
-                    width: screenWidth * 0.8,
-                    height: screenHeight * 0.6,
-                  ),
-                    );
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(
+                        screenWidth * 0.01, screenHeight * 0.01, screenWidth * 0.01, 0),
+                    width: screenWidth,
+                    child: ProfilePublicPostedCard(
+                      postName: posts[index].publisher?.username ?? "no name",
+                      description: posts[index].description ?? "no description",
+                      width: screenWidth * 0.8,
+                      height: screenHeight * 0.6,
+                      thumbnailUrl: posts[index].thumbnail ?? '',
+                    ),
+                  );
                 },
+              )
+                  : _isLoadingUsers
+                  ? Center(child: CircularProgressIndicator())
+                  : users.isEmpty
+                  ? Center(
+                child: Text(
+                  "No Users Found",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
               )
                   : ListView.builder(
                 itemCount: users.length,
                 itemBuilder: (context, index) {
-                  return
-                    Container(
-                      margin: EdgeInsets.fromLTRB(screenWidth * 0.01, screenHeight * 0.01, screenWidth * 0.01, 0),
-                      width: screenWidth,
-                      child:
-                        UserCard(profile: users[index], screenWidth: screenWidth, screenHeight: screenHeight),
-                    );
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(
+                        screenWidth * 0.01, screenHeight * 0.01, screenWidth * 0.01, 0),
+                    width: screenWidth,
+                    child: UserCard(
+                      profile: users[index],
+                      screenWidth: screenWidth,
+                      screenHeight: screenHeight,
+                    ),
+                  );
                 },
               ),
             ),
